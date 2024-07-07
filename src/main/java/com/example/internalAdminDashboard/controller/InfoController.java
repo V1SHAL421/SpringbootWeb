a531a5ab96e9d4d5fda1d891de18976d2311e11c
V1SHAL421 <vishalmkumar3@gmail.com>
2024-07-07 23:03:49 +0100

    feat: Add controllers and exception handling

--- src/main/java/com/example/internalAdminDashboard/controller/InfoController.java
+++ src/main/java/com/example/internalAdminDashboard/controller/InfoController.java
@@ -1,12 +1,63 @@
-//package com.example.internalAdminDashboard.controller;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class InfoController {
-    // This is for server-side rendering approach
+package com.example.internalAdminDashboard.controller;
+
+import com.example.internalAdminDashboard.model.User;
+import com.example.internalAdminDashboard.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/user")
+public class InfoController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+//     This is for client-side rendering approach
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+            return userRepository.findAll();
+
+    }
+
+    @GetMapping("/{name}")
+    public User getUserByName(@RequestParam String name) {
+        return userRepository.findUserByName(name);
+    }
+
+    @GetMapping("/age")
+    public List<User> getUsersByAge(@RequestParam Integer age) {
+        return userRepository.findUsersByAge(age);
+    }
+
+}
+
+
+
+
+
+
+
+//    public User getUserDetails(@RequestParam(name = "name", required = true), String name) {
+//        return new UserInfo(name);
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is for server-side rendering approach
 //    @GetMapping("/users")
 //    public String GetUserDetails(@RequestParam(name = "name", // Binds value of query string param into name param of method
 //    required = false, defaultValue = "Unavailable") String name, Model model) {
@@ -15,10 +66,3 @@
 //        model.addAttribute("name", name); // Value of name param added to model making it accessible to view template
 //        return "name"; // Returns name.html view relying on view tech (Thymeleaf). The name variable is passed through as well.
 //    }
-
-    // This is for client-side rendering approach
-//    @GetMapping("/users")
-//    public UserInfo getUserDetails(@RequestParam(name = "name", required = true), String name) {
-//        return new UserInfo(name);
-//    }
-//}
--- src/main/java/com/example/internalAdminDashboard/repository/UserRepository.java
+++ src/main/java/com/example/internalAdminDashboard/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.example.internalAdminDashboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.internalAdminDashboard.model.User;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByName(String name);
+    List<User> findUsersByAge(Integer age);
 }
