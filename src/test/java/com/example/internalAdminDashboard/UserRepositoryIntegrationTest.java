ddbc6266453b78d5e9450bbf5d442db2b56a87c3
V1SHAL421 <vishalmkumar3@gmail.com>
2024-07-10 23:13:48 +0100

    feat: Implement userRepository tests

--- src/test/java/com/example/internalAdminDashboard/UserRepositoryIntegrationTest.java
+++ src/test/java/com/example/internalAdminDashboard/UserRepositoryIntegrationTest.java
@@ -5,18 +5,22 @@ import com.example.internalAdminDashboard.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class UserRepositoryIntegrationTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserRepositoryIntegrationTest.class);
     @Autowired
     private UserRepository userRepository;
 
@@ -24,7 +28,51 @@ public class UserRepositoryIntegrationTest {
     public void saveOneUser() {
         userRepository.save(new User("Vishal", 20));
         List<User> users = (List<User>) userRepository.findAll();
+        LOGGER.info("The users returned are {}", users);
 
         assertThat(users.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void saveMultipleUsers() {
+        userRepository.save(new User("Vishal", 20));
+        userRepository.save(new User("Ryan", 30));
+        List<User> users = userRepository.findAll();
+        LOGGER.info("The users returned are {}", users);
+
+        assertThat(users.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void retrieveUserByName() {
+        userRepository.save(new User("Ben", 20));
+        userRepository.save(new User("Vishal", 20));
+        userRepository.save(new User("Ryan", 30));
+        User user = userRepository.findUserByName("Ryan");
+        LOGGER.info("The user returned is {}", user);
+
+        assertThat(user.getName()).isEqualTo("Ryan");
+    }
+
+    @Test
+    public void retrieveOneUserByAge() {
+        userRepository.save(new User("Ben", 20));
+        userRepository.save(new User("Vishal", 20));
+        userRepository.save(new User("Ryan", 30));
+        List<User> users = userRepository.findUsersByAge(30);
+        LOGGER.info("The users returned are {}", users);
+
+        assertThat(users.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void retrieveUsersByAge() {
+        userRepository.save(new User("Ben", 20));
+        userRepository.save(new User("Vishal", 20));
+        userRepository.save(new User("Ryan", 30));
+        List<User> users = userRepository.findUsersByAge(20);
+        LOGGER.info("The users returned are {}", users);
+
+        assertThat(users.size()).isEqualTo(2);
     }
 }
