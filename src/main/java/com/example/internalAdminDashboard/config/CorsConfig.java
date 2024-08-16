11617405530056d5728b0057657755ec3ab1aa56
V1SHAL421 <vishalmkumar3@gmail.com>
2024-08-16 21:38:20 +0100

    fix: Set allow credentials to false:

--- src/main/java/com/example/internalAdminDashboard/config/CorsConfig.java
+++ src/main/java/com/example/internalAdminDashboard/config/CorsConfig.java
@@ -13,6 +13,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
