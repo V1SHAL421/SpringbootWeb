e41bfe4b6f9e3246812ed7576a1a8efddbc730ab
V1SHAL421 <vishalmkumar3@gmail.com>
2024-08-16 21:58:31 +0100

    feat: Add CORS

--- src/main/java/com/example/internalAdminDashboard/config/CorsConfig.java
+++ src/main/java/com/example/internalAdminDashboard/config/CorsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfiguration.class);
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
